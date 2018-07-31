0c2f0893f7019f11c5a6972ea96f71bde744e3b8
vinayakkcn <vinayakkchandran@gmail.com>
2018-07-31 12:28:28 +1200

    changed

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-test
+var express = require('express');
+var app = express();
+
+app.get('/', function(req,res){
+res.send('Hello World');
+});
+
+app.listen(3000, function (){
+console.log('Sample App is listening port 3000');
+});
+
